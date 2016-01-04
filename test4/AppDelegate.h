7d4decf92e62a48951503390f234afe3f980f888
ykCoder <2842928569@qq.com>
2016-01-04 16:04:16 +0800

    Initial Commit

---
+++ test4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test4
+//
+//  Created by YKang on 16/1/4.
+//  Copyright © 2016年 YKang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test4
+//
+//  Created by YKang on 16/1/4.
+//  Copyright © 2016年 YKang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
